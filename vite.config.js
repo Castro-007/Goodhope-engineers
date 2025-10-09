8d45a157cba0787c840ebedfa469c4e777c12148
Castro-007 <sammythompson202@gmail.com>
2025-10-10 00:52:28 +0100

    added allowedhost

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', 
-    allowedHosts: ['topklan.onrender.com'],
+    allowedHosts: ['goodhopeengineers.onrender.com'],
     port: process.env.PORT || 5173,
   },
   assetsInclude: ['**/*.PNG'],
