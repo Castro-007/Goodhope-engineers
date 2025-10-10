e98eb62645b4ca8d2a849844087ea67fad829b1c
Castro-007 <sammythompson202@gmail.com>
2025-10-10 01:35:26 +0100

    made some camelCase changes

--- src/Components/Maps.jsx
+++ src/Components/Maps.jsx
@@ -8,9 +8,7 @@ const Maps = () => {
           <div className="w-full">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3964.50999148649!2d3.4787816!3d6.4568789!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103bf43516ca6171%3A0xff396563657a147f!2sZuken%20Apartments!5e0!3m2!1sen!2sng!4v1760051877762!5m2!1sen!2sng"
-              width="100%"
-              height="450"
-              style={{border:0}}
+              className="w-full h-[450px] border-0"
               allowFullScreen
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
